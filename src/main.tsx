2bfbe91c30d500a480caefc29d94c9628f193516
Jay Pawar <jayp90171@gmail.com>
2026-08-30 11:18:37 +0530

    added web analytics for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext.tsx'
+import { Analytics } from "@vercel/analytics/vue"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <ThemeProvider>
       <App />
     </ThemeProvider>
