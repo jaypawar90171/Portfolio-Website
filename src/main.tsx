4028b53a800d01a1b77a27d8563459bbecfea99a
Jay Pawar <jayp90171@gmail.com>
2026-08-30 11:23:09 +0530

    fixed issue of library import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext.tsx'
-import { Analytics } from "@vercel/analytics/vue"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
